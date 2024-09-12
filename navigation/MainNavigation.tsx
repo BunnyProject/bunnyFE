48fbffa52229729acdb2128e10e39ff3fcd1e207
gahyeon <117976216+gahyeon11@users.noreply.github.com>
2024-09-12 18:35:29 +0900

    [Feat] 스크린 이동을 위한 MainNavigation 구현 #1

---
+++ navigation/MainNavigation.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import LandingScreen from '../screens/LandingScreen';
+import UserInfoScreen from '../screens/UserInfoScreen';
+import UserInfo2Screen from '../screens/UserInfo2Screen';
+
+// Stack navigator 타입 정의
+export type RootStackParamList = {
+  Landing: undefined;
+  UserInfo: undefined;
+  UserInfo2: undefined;
+  UserInfo3: {
+    salaryType: string;
+    salary: string;
+    workDays: string[];
+    startTime: string;
+    endTime: string;
+  };
+};
+
+const Stack = createStackNavigator<RootStackParamList>();
+
+const MainNavigator: React.FC = () => {
+  return (
+    <Stack.Navigator initialRouteName="Landing">
+      <Stack.Screen name="Landing" component={LandingScreen} />
+      <Stack.Screen name="UserInfo" component={UserInfoScreen} />
+      <Stack.Screen name="UserInfo2" component={UserInfo2Screen} />
+      <Stack.Screen name="UserInfo3" component={UserInfo2Screen} />
+    </Stack.Navigator>
+  );
+};
+
+export default MainNavigator;
